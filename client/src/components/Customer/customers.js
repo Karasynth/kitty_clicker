33bea5eb80f57cab23b766f41d9df751f0d4cd8c
Kara <thomsos.prod@gmail.com>
2021-09-17 13:28:00 -0400

    WIP

--- client/src/components/Customer/customers.js
+++ client/src/components/Customer/customers.js
@@ -4,6 +4,10 @@ import {connect } from 'react-redux';
 import {getCustomers} from '../../store/actions/customer'
 import './customers.css';
 
+import useSound from 'use-sound';
+
+import catMeow3 from '../../sounds/cat-meow-3.wav';
+
 class Customers extends Component {
 
   static propTypes = {
@@ -21,12 +25,20 @@ class Customers extends Component {
 
   render() {
 
+    const [play] = useSound(catMeow3)
+
     return (
       <div>
         <h2>Customers</h2>
         <ul>
         {this.props.customers.map(customer =>
-          <li key={customer.id}>{customer.firstName} {customer.lastName}</li>
+          <li key={customer.id}>
+            {customer.firstName} {customer.lastName}
+            <img 
+              src={customer.url}
+              onClick={play}
+            />
+          </li>
         )}
         </ul>
       </div>
--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.get('/api/customers', (req, res) => {
   const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+    {id: 1, firstName: 'Ashley', lastName: 'Cat', url: 'https://images.unsplash.com/photo-1492370284958-c20b15c692d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80'},
+    {id: 2, firstName: 'Laura', lastName: 'Kitty', url: 'https://images.unsplash.com/photo-1536590158209-e9d615d525e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'},
+    {id: 3, firstName: 'Alstofo', lastName: 'Nya', url: 'https://images.unsplash.com/photo-1598188306155-25e400eb5078?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80'},
   ];
 
   res.json(customers);
